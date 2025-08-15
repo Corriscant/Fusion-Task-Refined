7c62f615e9060ad587a2356c64d4cbbe6f6578d2
Corris <corriscant@gmail.com>
2025-08-15 11:54:05 +0300

    add MainMenu in Inspector

--- Assets/Scripts/GameLauncher.cs
+++ Assets/Scripts/GameLauncher.cs
@@ -26,9 +26,10 @@ public class GameLauncher : MonoBehaviour
     [SerializeField] private Color buttonColor = new Color(1f, 1f, 1f, 0.717f);
     [SerializeField] private Vector2 menuOffset = new Vector2(5f, 5f); // Offset from top-left corner
 
-    private Button _hostButton;
-    private Button _joinButton;
-    private Button _exitButton;
+    [SerializeField] private Button hostButton;
+    [SerializeField] private Button joinButton;
+    [SerializeField] private Button exitButton;
+
     private Button[] _buttons;
     #endregion GUI
 
@@ -50,11 +51,12 @@ public class GameLauncher : MonoBehaviour
             return;
         }
 
-        CreateMenu();
+        StartMenu();
     }
 
-    private void CreateMenu()
+    private void StartMenu()
     {
+        /*
         var canvasGo = new GameObject("CanvasMenu");
         var canvas = canvasGo.AddComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceOverlay;
@@ -64,12 +66,19 @@ public class GameLauncher : MonoBehaviour
         _hostButton = CreateButton(canvas.transform, new Vector2(menuOffset.x, -menuOffset.y), "Host");
         _joinButton = CreateButton(canvas.transform, new Vector2(menuOffset.x, -(menuOffset.y + buttonHeight + buttonSpacing)), "Join");
         _exitButton = CreateButton(canvas.transform, new Vector2(menuOffset.x, -(menuOffset.y + 2 * (buttonHeight + buttonSpacing))), "Exit");
+        */
 
-        _buttons = new[] { _hostButton, _joinButton, _exitButton };
+        if (hostButton == null || joinButton == null || exitButton == null)
+        {
+            LogError($"{GetLogCallPrefix(GetType())} Buttons are not assigned in the inspector!");
+            return;
+        }
 
-        _hostButton.onClick.AddListener(async () => await _connectionService.StartGame(GameMode.Host));
-        _joinButton.onClick.AddListener(async () => await _connectionService.StartGame(GameMode.Client));
-        _exitButton.onClick.AddListener(QuitGame);
+        _buttons = new[] { hostButton, joinButton, exitButton };
+
+        hostButton.onClick.AddListener(async () => await _connectionService.StartGame(GameMode.Host));
+        joinButton.onClick.AddListener(async () => await _connectionService.StartGame(GameMode.Client));
+        exitButton.onClick.AddListener(QuitGame);
     }
 
     private Button CreateButton(Transform parent, Vector2 anchoredPos, string text)
@@ -114,13 +123,13 @@ public class GameLauncher : MonoBehaviour
         bool selectionActive = SelectionManager.IsSelecting;
         bool runnerMissing = _connectionService.Runner.IsNullOrDestroyed();
 
-        _hostButton.gameObject.SetActive(runnerMissing);
-        _joinButton.gameObject.SetActive(runnerMissing);
+        hostButton.gameObject.SetActive(runnerMissing);
+        joinButton.gameObject.SetActive(runnerMissing);
 
         bool interactable = !_connectionService.IsConnecting && !selectionActive;
-        _hostButton.interactable = interactable;
-        _joinButton.interactable = interactable;
-        _exitButton.interactable = !selectionActive;
+        hostButton.interactable = interactable;
+        joinButton.interactable = interactable;
+        exitButton.interactable = !selectionActive;
 
         UpdateMenuLayout();
     }
