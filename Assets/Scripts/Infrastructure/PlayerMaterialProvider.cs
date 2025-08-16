8d4a524a45e52ef89ac668b9c21763b5297a6cb6
Corris <corriscant@gmail.com>
2025-08-16 18:55:43 +0300

    addressable migration WIP

--- Assets/Scripts/Infrastructure/PlayerMaterialProvider.cs
+++ Assets/Scripts/Infrastructure/PlayerMaterialProvider.cs
@@ -1,5 +1,8 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace FusionTask.Infrastructure
 {
@@ -8,8 +11,23 @@ namespace FusionTask.Infrastructure
     /// </summary>
     public static class PlayerMaterialProvider
     {
+        private const string MaterialListAddress = "PlayerMaterialList.mat";
+        private static AsyncOperationHandle<PlayerMaterialList> _handle;
+
         private static readonly Dictionary<int, Material> _materials = new();
         private static PlayerMaterialList _materialList;
+
+        public static async Task<Material> GetMaterialAsync(int index)
+        {
+            if (!_handle.IsValid())
+                _handle = Addressables.LoadAssetAsync<PlayerMaterialList>(MaterialListAddress);
+
+            _materialList = await _handle.Task;
+            if (_materialList == null || index < 0 || index >= _materialList.Materials.Count)
+                return null;
+
+            return _materialList.Materials[index];
+        }
 
         /// <summary>
         /// Returns a material for the given index. The material is loaded once and then cached.
