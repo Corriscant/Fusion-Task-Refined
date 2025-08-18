f8ddcd0565afbd1f95af1dcda3de3ec51804c335
Corris <corriscant@gmail.com>
2025-08-18 17:39:19 +0300

    removed unneeded NTests

--- Assets/Tests/Editor/UnitMathTests.cs
+++ Assets/Tests/Editor/UnitMathTests.cs
@@ -28,29 +28,6 @@ namespace FusionTask.Tests.Editor
         }
 
         [Test]
-        public void ClampOffset_ClampsMagnitudeCorrectly()
-        {
-            Vector3 offset = new Vector3(3f, 0f, 4f);
-            float allowed = 3f;
-
-            Vector3 result = _unit.ClampOffset(offset, allowed);
-
-            Assert.AreEqual(allowed, result.magnitude, 0.001f);
-        }
-
-        [Test]
-        public void GetUnitTargetPosition_OffsetsTargetAsExpected()
-        {
-            Vector3 offset = new Vector3(1f, 0f, -2f);
-            Vector3 groupTarget = new Vector3(5f, 0f, 10f);
-            Vector3 expected = groupTarget - offset;
-
-            Vector3 result = _unit.GetUnitTargetPosition(offset, groupTarget);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [Test]
         public void HasReachedTarget_ReturnsTrueWithinOneUnitRadiusXZ()
         {
             Vector3 target = new Vector3(5f, 0f, 5f);
